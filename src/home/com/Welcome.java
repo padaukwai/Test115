7abbf2ee71e0f5b96ec2c7055720db960c11b01c
IMU <IMU@DESKTOP-FSRQE0S>
2024-09-02 11:03:47 +0630

    Version 1

---
+++ src/home/com/Welcome.java
@@ -0,0 +1,11 @@
+package home.com;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome from IMU University");
+
+	}
+
+}
