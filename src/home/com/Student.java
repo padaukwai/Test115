667e689025f79a42cb18f173bbf5d2afc34d16c9
IMU <IMU@DESKTOP-FSRQE0S>
2024-09-02 11:27:15 +0630

    Version2

---
+++ src/home/com/Student.java
@@ -0,0 +1,14 @@
+package home.com;
+
+public class Student {
+	String stname;
+	Student(String stu_name)
+	{
+		stname=stu_name;
+	}
+	public void getStuName()
+	{
+		System.out.println("Student Name is "+stname);
+	}
+
+}
